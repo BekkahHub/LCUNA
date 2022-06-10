e04fa767de5c7bc7156cda7f20f50c45ce5534da
dcoder-x <ayomikunfaluyi@gmail.com>
2022-06-10 09:31:49 +0100

    home page added

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const navBar = document.querySelector('.nav-bar')
+const menu = document.querySelectorAll('.menu')
+
+
+window.onscroll = (e)=>{
+    const scrollTop =document.documentElement.scrollTop
+     if (scrollTop>100) {
+         navBar.classList.add('scrolled')
+         menu.forEach((menu)=>{
+            menu.style.color='black'
+         })
+     } else {
+        navBar.classList.remove('scrolled')
+        menu.forEach((menu)=>{
+            menu.style.color='white'
+         })
+     }
+}
+menu.forEach((menu)=>{
+    menu.onmouseover=(e)=>{
+        e.preventDefault()
+        e.target.firstElementChild.style.transform='scale(1)'
+    }
+    menu.onmouseleave=(e)=>{
+        e.preventDefault()
+        e.target.firstElementChild.style.transform='scale(0)'
+    }
+ })
