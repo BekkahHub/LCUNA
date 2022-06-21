704fa9ce8bb1cf9f1bf044eff904c1a6f8ec3dec
dcoder-x <ayomikunfaluyi@gmail.com>
2022-06-21 12:38:31 +0100

    created reusable components with htmlreusable library

---
+++ scripts/navbar.js
@@ -0,0 +1,46 @@
+const navbar= `    
+<div class="nav-bar">
+        <div class="logo">
+            <a href="../index.html">
+                <img src="../images/LOGO.svg" alt="logo" />
+            </a>
+        </div>
+        <div class="menu-holder">
+            <a href="/pages/about.html">
+                <div class="menu">
+                    About Us
+                    <hr>
+                </div>  
+            </a>
+            <a href="/">
+                <div class="menu">
+                    Media
+                    <hr>
+                </div>  
+            </a>
+            <a href="../pages/FAQ.html">
+                <div class="menu">
+                    FAQ
+                    <hr>
+                </div>  
+            </a>
+            <a href="../pages/history.html">
+                <div class="menu">
+                    History
+                    <hr>
+                </div>  
+            </a>
+        </div>
+        <a href="/pages/contact.html">
+            <div class="contact-button">
+                <div class="contact-btn-bg">
+    
+                </div>
+                Contact us
+            </div>
+        </a>
+        </div>
+
+`
+
+reuse('nav-bar',navbar)
