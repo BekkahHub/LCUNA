6dc1791e90632427466d3a33aa609bb4d7529326
dcoder-x <ayomikunfaluyi@gmail.com>
2022-06-23 15:18:02 +0100

    pages complete and responsive

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,20 +1,24 @@
 const navbar= `    
 <link rel="stylesheet" href="../styles/nav.css">
-<header>
-<a class="logo" href="/index.html">
-    <img src="../images/LOGO.png" alt="logo">
-</a>
-<nav>
-    <ul>
-        <li><a href="../pages/about.html">About Us</a></li>
-        <li><a href="../pages/media.html">Media</a></li>
-        <li><a href="../pages/FAQ.html">FAQ</a></li>
-        <li><a href="../pages/history.html">History</a></li>
-        <li><a href="../pages/contact.html">Contact Us</a></li>
-    </ul>
-</nav>
-</header>
-
+    <header>
+        <a class="logo" href="/index.html">
+            <img src="../images/LOGO.png" alt="logo">
+        </a>
+        <nav>
+            <div class="menu-button">
+                <div class="menu-line"></div>
+                <div class="menu-line"></div>
+                <div class="menu-line"></div>
+            </div>
+            <ul>
+                <li><a href="../pages/about.html">About Us</a></li>
+                <li><a href="../pages/media.html">Media</a></li>
+                <li><a href="../pages/FAQ.html">FAQ</a></li>
+                <li><a href="../pages/history.html">History</a></li>
+                <li><a href="../pages/contact.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
 `
 
-reuse('nav-bar',navbar)
+reuse('nav-bar',navbar,false,true)
