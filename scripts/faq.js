d02bea91f6c9e1da47b5ff518d55280accb08656
omobankole <oluwabankyusuf@gmail>
2022-07-03 22:44:42 +0100

    accordion changes

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,16 +1,13 @@
-const accordion= document.querySelectorAll('.accordion') 
-const accordionBtn= document.querySelectorAll('.accordion-btn') 
+const accordion = document.querySelectorAll(".accordion");
+const accordionBtn = document.querySelectorAll(".accordion-btn");
 
-accordion.forEach((element,index) => {
-    element.onclick=()=>{
-       
-        element.classList.toggle('open')
-        if (element.clientHeight<70) {
-            accordionBtn[index].innerHTML='&minus;'
-        }
-        else{
-            accordionBtn[index].innerHTML='&plus;'
-        }
+accordion.forEach((element, index) => {
+  element.onclick = () => {
+    element.classList.toggle("open");
+    if (element.clientHeight <= 70) {
+      accordionBtn[index].innerHTML = "&minus;";
+    } else {
+      accordionBtn[index].innerHTML = "&plus;";
     }
+  };
 });
-
