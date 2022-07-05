f710bd7daea036ddeda78e7084ea7d8c57ef4eeb
omobankole <oluwabankyusuf@gmail>
2022-07-05 02:39:46 +0100

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,7 @@ const menu = document.querySelectorAll('.menu')
 const content = document.querySelectorAll('.content')
 
 
-// window.onscroll = (e)=>{
-//     const scrollTop =document.documentElement.scrollTop
-//      if (scrollTop>100) {
-//          navBar.classList.add('scrolled')
-//          menu.forEach((menu)=>{
-//             menu.style.color='black'
-//          })
-//      } else {
-//         navBar.classList.remove('scrolled')
-//         menu.forEach((menu)=>{
-//             menu.style.color='white'
-//          })
-//      }
-// }
+
 menu.forEach((menu)=>{
     menu.onmouseover=(e)=>{
         e.preventDefault()
@@ -28,28 +15,27 @@ menu.forEach((menu)=>{
         e.target.firstElementChild.style.transform='scale(0)'
     }
  });
-
-
-
-
- $(function() {
-    AOS.init();
-    window.addEventListener('load', AOS.refresh);
-  });
  
 
  
-//  const observer = new IntersectionObserver((entries)=>{
-//      entries.forEach((entry)=>{
-//         if (entry.isIntersecting==true) {
-//             entry.target.classList.add('inview')
-//           }
-//         else{
-//             entry.target.classList.remove('inview')
-//         }
-//      })
-//  })
-//  content.forEach((content)=>{
-//     observer.observe(content)
-//  })
+// slideshow
  
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+};
