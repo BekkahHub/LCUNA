5dc2d64d915a6b644985df849fd1d88abb5a31ee
dcoder-x <ayomikunfaluyi@gmail.com>
2022-07-08 13:51:34 +0100

    edited dropdown logic in menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -69,13 +69,13 @@
 
 
   // search-box open close js code
-  let navbar = document.querySelector(".navbar");
+  let navBar = document.querySelector(".navbar");
   let searchBox = document.querySelector(".search-box .bx-search");
   // let searchBoxCancel = document.querySelector(".search-box .bx-x");
   
   searchBox.addEventListener("click", ()=>{
-    navbar.classList.toggle("showInput");
-    if(navbar.classList.contains("showInput")){
+    navBar.classList.toggle("showInput");
+    if(navBar.classList.contains("showInput")){
       searchBox.classList.replace("bx-search" ,"bx-x");
     }else {
       searchBox.classList.replace("bx-x" ,"bx-search");
